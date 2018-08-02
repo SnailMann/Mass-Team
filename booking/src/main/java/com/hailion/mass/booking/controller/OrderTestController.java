366e46e1f0fb286b8f90702f27ff10409cdb68be
SnailMann <453001674@qq.com>
2018-08-02 09:38:33 +0800

    update

--- booking/src/main/java/com/hailion/mass/booking/controller/OrderTestController.java
+++ booking/src/main/java/com/hailion/mass/booking/controller/OrderTestController.java
@@ -1,26 +1,12 @@
 package com.hailion.mass.booking.controller;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
+
 import java.util.ArrayList;
-import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.hailion.mass.booking.client.PaymentClient;
+import org.springframework.web.bind.annotation.*;
 import com.hailion.mass.booking.entity.Order;
-import com.hailion.mass.booking.entity.OrderDO;
 import com.hailion.mass.booking.service.OrderService;
 
 
@@ -35,8 +21,7 @@ public class OrderTestController {
     *  跳转到订单页面
     * @return
     */
-
-	@RequestMapping("/booking/orderRequest")
+	@GetMapping("/booking/orderRequest")
 	public String orderRequest() {
 		return "insertOrder";
 	}
@@ -46,21 +31,36 @@ public class OrderTestController {
         orderService.insertOrder(order);
         return "redirect:/booking/list";
     }
-	//查询订单
+
+    /**
+     * 查询订单
+     * @param request
+     * @return
+     */
 	@GetMapping("/booking/list")
     public String listOrder(HttpServletRequest request){
         ArrayList<Order> orderList = (ArrayList<Order>) orderService.listOrder();
         System.out.println(orderList);
-       request.setAttribute("orderList", orderList);
+        request.setAttribute("orderList", orderList);
         return "orderList";
     }
-    //	取消订单
+
+    /**
+     * 取消订单
+     * @param order
+     * @return
+     */
 	@GetMapping("/booking/cancel")
     public String cancelOrder(Order order){
 		orderService.updateOrder(order);
        return "redirect:/booking/list";
     }
-	//删除订单
+
+    /**
+     * 删除订单
+     * @param order
+     * @return
+     */
 	 @GetMapping("/booking/delete")
 	    public String deleteAnOrder(Order order){
 	        orderService.deleteOrderById(order.getOrderId());
